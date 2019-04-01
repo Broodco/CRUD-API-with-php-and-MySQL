4e6579a404bcd38316045fbe32d1cf97516a81f5
Broodco <broodcoorens.maxime@gmail.com>
2019-04-01 16:59:22 +0200

    changed create.php CORS policy

--- contacts/create.php
+++ contacts/create.php
@@ -4,7 +4,7 @@
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Origin, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     // Includes
     include_once '../config/database.php';
     include_once '../objects/contacts.php';
